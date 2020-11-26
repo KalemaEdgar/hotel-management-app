7853725d7f07a334e102b5747b62ca426d5f931b
Kalema Edgar <eked290@gmail.com>
2020-11-26 18:07:07 +0300

    Added a datepicker, UI changes with bootstrap and search functionality

--- src/main/java/com/home/lil/learningspring/business/service/ReservationService.java
+++ src/main/java/com/home/lil/learningspring/business/service/ReservationService.java
@@ -1,5 +1,12 @@
 package com.home.lil.learningspring.business.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.home.lil.learningspring.business.domain.RoomReservation;
 import com.home.lil.learningspring.data.entity.Guest;
 import com.home.lil.learningspring.data.entity.Reservation;
@@ -9,8 +16,6 @@ import com.home.lil.learningspring.data.repository.ReservationRepository;
 import com.home.lil.learningspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class ReservationService {
@@ -25,7 +30,7 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
-    public List<RoomReservation> getRoomReservationsForDate(Date date) {
+    public List<RoomReservation> getRoomReservationsForDate(Date date){
         Iterable<Room> rooms = this.roomRepository.findAll();
         Map<Long, RoomReservation> roomReservationMap = new HashMap();
         rooms.forEach(room -> {
@@ -35,7 +40,6 @@ public class ReservationService {
             roomReservation.setRoomNumber(room.getRoomNumber());
             roomReservationMap.put(room.getRoomId(), roomReservation);
         });
-
         Iterable<Reservation> reservations = this.reservationRepository.findReservationByReservationDate(new java.sql.Date(date.getTime()));
         reservations.forEach(reservation -> {
             RoomReservation roomReservation = roomReservationMap.get(reservation.getRoomId());
@@ -45,12 +49,19 @@ public class ReservationService {
             roomReservation.setLastName(guest.getLastName());
             roomReservation.setGuestId(guest.getGuestId());
         });
-
         List<RoomReservation> roomReservations = new ArrayList<>();
         for(Long id: roomReservationMap.keySet()){
             roomReservations.add(roomReservationMap.get(id));
         }
-
+        roomReservations.sort(new Comparator<RoomReservation>() {
+            @Override
+            public int compare(RoomReservation o1, RoomReservation o2) {
+                if (o1.getRoomName() == o2.getRoomName()){
+                    return o1.getRoomNumber().compareTo(o2.getRoomNumber());
+                }
+                return o1.getRoomName().compareTo(o2.getRoomName());
+            }
+        });
         return roomReservations;
     }
 }
