cc9f865788f2d7a9ad708b86d2730d7ed9dc4cb3
Kalema Edgar <eked290@gmail.com>
2020-11-26 17:36:03 +0300

    Initial commit

---
+++ src/main/java/com/home/lil/learningspring/data/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package com.home.lil.learningspring.data.repository;
+
+import com.home.lil.learningspring.data.entity.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
---
+++ src/main/java/com/home/lil/learningspring/web/RoomReservationWebController.java
@@ -0,0 +1,4 @@
+package com.home.lil.learningspring.web;
+
+public class RoomReservationWebController {
+}
